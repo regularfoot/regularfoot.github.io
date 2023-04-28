cedba3f3def9315ab07828d6eb22e12b1bc104b1
maxshishkin <mshishkin@more.tv>
2023-04-28 14:53:25 +0300

    add ACM page

---
+++ acm/dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - ACM Page v1.0.0 (undefined)
+* Copyright 2013-2023 undefined
+* Licensed under undefined (https://github.com/StartBootstrap/ACM/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
